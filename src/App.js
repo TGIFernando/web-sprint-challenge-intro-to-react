f5b72c379ccd7e83e17fb2fac161f5bd497e2f17
Fernando Chavez <fjchavez01@gmail.com>
2020-10-09 10:16:57 -0700

    got cards working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios'
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +11,31 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [rmData, setRMData] = useState([{}])
+  const BASE_URL = 'https://rickandmortyapi.com/api/character/?page='
+
+  useEffect(() => {
+    axios.get(`
+    ${BASE_URL}
+    `).then(res => {
+      setRMData(res.data.results)
+    }).catch(err => {
+      console.log('ERROR: ',err)
+    })
+  }, [])
+
+  const eh = rmData.map(x => x)
+  console.log('console 3: ', eh[0].image)
+  for(let i = 0; i < eh.length; i++){
+    console.log(eh[i].name)
+  }
+
+  
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character img = {eh[0].image} name = {eh[0].name} gen = {eh[0].gender}></Character>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,32 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+
+function Character(props){
+    const {img} = props
+    const {name} = props
+    const {gen} = props
+    const MainDiv = styled.div`
+    border: 2px solid blue;
+    background-color: rgba(50, 115, 220, 0.3);
+    width: 45%;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    `
+
+    return(
+        <MainDiv>
+            <div>
+            <h2>{name}</h2>
+            <div>
+                <img src = {img}></img>
+            </div>
+            <h3>{gen}</h3>
+            </div>
+        </MainDiv>
+    )
+}
+
+export default Character
